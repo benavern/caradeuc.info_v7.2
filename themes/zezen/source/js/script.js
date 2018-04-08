f2021fc0fc1e176745f0b6cc101b0f436fe619f0
Benjamin Caradeuc <bencrdc@gmail.com>
2018-04-09 00:16:39 +0200

    Add contact page layout

--- themes/zezen/source/js/script.js
+++ themes/zezen/source/js/script.js
@@ -1,5 +1,10 @@
 (function(window, document, undefined) {
 
+  // --- variables
+  var inputFields = [].slice.call(document.querySelectorAll('.input input, .input textarea'))
+
+  // --- functions
+
   function handleScrolled () {
     if (window.scrollY > 0) {
       document.body.classList.add('scrolled')
@@ -8,7 +13,25 @@
     }
   }
 
+  function handleInputFields (e) {
+    if(e && e.type === 'focus') {
+      this.parentNode.classList.add('is-focus', 'has-value')
+    } else { // blur...
+      this.parentNode.classList.remove('is-focus')
+      if (!this.value) {
+        this.parentNode.classList.remove('has-value')
+      }
+    }
+  }
+
+  // --- events
   window.addEventListener('scroll', handleScrolled)
+  inputFields.forEach(input => {
+    input.addEventListener('focus', handleInputFields)
+    input.addEventListener('blur', handleInputFields)
+  })
+
+  // --- init
   handleScrolled()
 
 })(window, document)
