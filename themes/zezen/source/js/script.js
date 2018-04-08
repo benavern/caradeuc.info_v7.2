cf6ee4b522b330224e75d49e8e4b72d23ccae797
Benjamin Caradeuc <bencrdc@gmail.com>
2018-04-08 13:07:14 +0200

    Simplification navigation script

--- themes/zezen/source/js/script.js
+++ themes/zezen/source/js/script.js
@@ -1,14 +1,10 @@
 (function(window, document, undefined) {
 
-  var body = document.body
-  var fontSize = parseInt(window.getComputedStyle(document.body).getPropertyValue('font-size')) || 16
-
   function handleScrolled () {
-    var scrolled = this.scrollY > (2 * fontSize)
-    if (scrolled) {
-      body.classList.add('scrolled')
+    if (window.scrollY > 0) {
+      document.body.classList.add('scrolled')
     } else {
-      body.classList.remove('scrolled')
+      document.body.classList.remove('scrolled')
     }
   }
 
