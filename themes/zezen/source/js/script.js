d356f91c082a8fbd22d8c0f27f76cb46d2d93d4c
Benjamin Caradeuc <bencrdc@gmail.com>
2018-04-17 00:28:00 +0200

    Add pop behaviour

--- themes/zezen/source/js/script.js
+++ themes/zezen/source/js/script.js
@@ -90,8 +90,33 @@
     }
   }
 
-  window.addEventListener('scroll', handleStickyAside, 50)
-  window.addEventListener('resize', handleStickyAside, 50)
+  window.addEventListener('scroll', handleStickyAside)
+  window.addEventListener('resize', handleStickyAside)
   handleStickyAside()
+
+  /**
+   * Pop elements on the page
+   */
+
+  function handlePop () {
+
+    var popElements = [].slice.call(document.querySelectorAll('.pop'))
+    var currentScroll = window.scrollY
+    var isBottom = currentScroll >= document.body.scrollHeight - window.innerHeight
+    var popHeight = Math.max(window.innerHeight, currentScroll + (0.8 * window.innerHeight))
+    popElements.forEach(el => {
+      if (popHeight > el.offsetTop || isBottom) {
+        el.classList.add('popped')
+      } else {
+        el.classList.remove('popped')
+      }
+    })
+  }
+
+  window.addEventListener('scroll', handlePop)
+  window.addEventListener('resize', handlePop)
+  handlePop()
+
+
 
 })(window, document)
