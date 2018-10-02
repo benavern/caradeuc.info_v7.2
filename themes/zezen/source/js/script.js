6bcb3d9f1102ce145d13606ae22385736d68b531
Benjamin Caradeuc <bencrdc@gmail.com>
2018-10-03 01:30:26 +0200

    my age is now dynamic on homepage

--- themes/zezen/source/js/script.js
+++ themes/zezen/source/js/script.js
@@ -151,6 +151,31 @@
   window.addEventListener('resize', handlePop)
   handlePop()
 
+  /**
+   * counts my age... outch!
+   */
+  function handleMyAge() {
+    var target = document.querySelector('.my-age')
+
+    if(target) {
+      var now = new Date()
+      var birth = { y: 1990, m: 09, d: 27 }
+      var myAge = {
+        y: now.getFullYear() - birth.y,
+        m: (now.getMonth() + 1) - birth.m,
+        d: now.getDate() - birth.d
+      }
+
+      var ageToDisplay = myAge.y
+
+      // before birthday in the year?
+      if (myAge.m === 0 && myAge.d > 0 || myAge.m < 0) ageToDisplay -= 1
+
+      target.innerHTML = ageToDisplay
+    }
+  }
+
+  handleMyAge()
 
 
 })(window, document)
