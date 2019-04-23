e7af61feced792147d53b60a83b429317684dae6
Benjamin Caradeuc <bencrdc@gmail.com>
2019-04-23 19:57:12 +0200

    get the medium image overlay color to follow the theme

--- themes/zezen/source/js/script.js
+++ themes/zezen/source/js/script.js
@@ -148,11 +148,12 @@
   /**
    * zoom on images of blog articles
    */
-  mediumZoom(document.querySelectorAll('article.post-content p > img'), {
+  const zoom = mediumZoom(document.querySelectorAll('article.post-content p > img'), {
     margin: window.innerWidth < 1024 ? 8 : 32,
     container: {
       top: navigation.offsetHeight
-    }
+    },
+    background: 'var(--gray-lighter)'
   })
 
 
