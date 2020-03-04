6b4a1bc1a7f78b6c4764f3f37f56b543399c7943
Benjamin Caradeuc <bencrdc@gmail.com>
2020-03-04 14:22:29 +0100

    Fix the pop algo

--- themes/zezen/source/js/_scripts/pop.js
+++ themes/zezen/source/js/_scripts/pop.js
@@ -8,15 +8,44 @@ export default function () {
 }
 
 function handlePop () {
-  var popElements = [].slice.call(document.querySelectorAll('.pop'))
-  var currentScroll = window.scrollY
-  var isBottom = currentScroll >= document.body.scrollHeight - window.innerHeight
-  var popHeight = Math.max(window.innerHeight, currentScroll + (0.8 * window.innerHeight))
-  popElements.forEach(el => {
-    if (popHeight > el.offsetTop || isBottom) {
+  // the elements to perform the pop action
+  const popElements = [].slice.call(document.querySelectorAll('.pop'))
+
+  // the current window scroll
+  const currentScroll = window.scrollY
+
+  // is the window scrolled to the bottom ?
+  const isScrolledToBottom = currentScroll >= document.body.scrollHeight - window.innerHeight
+
+  // the height where the elements shoold be popped
+  const popHeight = Math.max(window.innerHeight, currentScroll + (0.8 * window.innerHeight))
+
+  // go through all the elements found earlier and then ...
+  popElements.forEach((el, i) => {
+    // if element is above the limit or if the page is fully scrolled, apply the `popped` class
+    // otherwise, remove it.
+    if (isScrolledToBottom || getOffsetTop(el) < popHeight) {
       el.classList.add('popped')
     } else {
       el.classList.remove('popped')
     }
   })
+}
+
+/**
+ * Get the element distance from the top of the document by traversing the dom
+ * @param {Element} el
+ */
+function getOffsetTop(el) {
+  let offsetTop = 0
+  let node = el
+
+  while (node) {
+    if (!isNaN(node.offsetTop)) {
+			offsetTop += node.offsetTop
+		}
+		node = node.offsetParent
+  }
+
+  return offsetTop
 }
